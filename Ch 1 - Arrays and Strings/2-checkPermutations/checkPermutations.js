3de651a6621f4d06d64c025099e0b61aeb9f39f6
Austin Killough <austinbkillough@gmail.com>
2021-04-21 11:00:47 -0700

    Check Permutations complete

--- Ch 1 - Arrays and Strings/2-checkPermutations/checkPermutations.js
+++ Ch 1 - Arrays and Strings/2-checkPermutations/checkPermutations.js
@@ -1,7 +1,20 @@
 //Given two strings, write a method to decide if one is a permutation of the other.
-
+// racecar, e
 const checkPermutations = (str1, str2) => {
+  //copy the string for mutation
+  let copy2 = str2.slice();
   //Please code here
+  let counter = 0;
+  for (let i = 0; i < str1.length; i++) {
+    if (copy2.includes(str1[i])) {
+      counter++;
+      copy2 = copy2.replace(str1[i], '');
+    }
+  }
+  if (counter === str1.length && copy2.length === 0) {
+    return true
+  }
+  return false
 };
 
 module.exports = checkPermutations;
